d163f1c3b3e3f3a8e2ccf7bc89dad150adb03c9e
youyou_pm10 <1805612892@qq.com>
2022-08-16 12:18:18 +0800

    update third

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python#-*-coding:utf-8-*-
-"""初始化"""
-def init_start():
-    pass
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+import set_map
+
+class game:
+    """初始化"""
+    def init_start():
+        pass
+
+class map_init(set_map):
+    def __init__(self):
+        super(map, self).__init__()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = map_init()
+    w.show()
+    app.exec()
