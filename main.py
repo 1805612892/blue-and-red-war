347a52f8cd275726e3642742b23e1c0c5f145fb0
cliffigor <jitianxu25@gmail.com>
2022-08-17 19:42:39 +0800

    rename Class, format code

--- main.py
+++ main.py
@@ -4,120 +4,146 @@ import random
 
 from PyQt5.QtWidgets import QApplication
 
-from map import Set_map
+from map import SetMap
 
-"""游戏主体"""
-class game():
 
-    """初始化"""
+class Game():
+    """游戏主体"""
+
     def __init__(self):
-        self.land = map_init()
+        """初始化"""
+        self.land = MapInit()
         self.init_extend_map()
-        self.player = player(2)
+        self.player = Player(2)
         self.listen()
 
-
-    """扩展地图初始化"""
     def init_extend_map(self):
+        """
+        扩展地图初始化
+        """
         self.land.define_terrain()
-        #禁止通过地形
+        # 禁止通过地形
         self.disable = []
         self.chess = self.land.chess_name
         self.check(sign=1)
 
-    """建立鼠标监听事件"""
     def listen(self):
+        """
+        建立鼠标监听事件
+        """
         for button in self.land.cache:
             button.clicked.connect(self.record)
 
-    """判断点击来源"""
     def record(self):
+        """
+        判断点击来源
+        """
         send = self.land.sender()
         self.chess = send.text()
         self.check()
 
-    """检查移动条件"""
-    def check(self,sign = 0):
+    def check(self, sign=0):
+        """
+        检查移动条件
+        :param sign: 0->人类棋子; 1->高山
+        """
         site = None
-        premit = None
+        permit = None
         for i in range(len(self.land.cache)):
             if self.land.cache[i].text() == self.chess:
                 site = i
-                #遍历禁止地形
+                # 遍历禁止地形
                 for disable in self.disable:
                     if self.land.cache[i] == disable:
                         site = None
-        #安全检查通过，允许进军
+        # 安全检查通过，允许进军
         if site != None:
-            #检查为人类棋子
+            # 检查为人类棋子
             if sign == 0:
-                #premit控制，还没写
+                # permit控制，还没写
                 if True:
                     self.march(site)
-            #检查为高山地形
+            # 检查为高山地形
             if sign == 1:
-                self.flag(self.land.cache[site],'green')
+                self.flag(self.land.cache[site], 'green')
                 self.disable.append(self.land.cache[site])
 
-    """棋子移动"""
-    def march(self,site):
-        #保证交替下棋
+    def march(self, site):
+        """
+        棋子移动
+        :param site: 
+        """
+        # 保证交替下棋
         color = self.player.flag[self.player.id[self.player.round_now]]
         if site != None:
-        #持续隐藏字体
-            self.flag(self.land.cache[site],color)
-        #储存玩家下的棋子的位置
-            self.player.chess_map[self.player.round_now].append(self.land.cache[site].text())
+            # 持续隐藏字体
+            self.flag(self.land.cache[site], color)
+        # 储存玩家下的棋子的位置
+            self.player.chess_map[self.player.round_now].append(
+                self.land.cache[site].text())
         self.player.player_round()
 
-    """棋子染色"""
-    def flag(self,object,color):
-        object.setStyleSheet("color : rgb(0,0,0,0);\n"
-                                 "background-color : {}".format(color))
+    def flag(self, chess, color):
+        """
+        棋子染色
+        :param chess: 要染色的棋子
+        :param color: 染后的颜色
+        """
+        chess.setStyleSheet("color : rgb(0,0,0,0);\n"
+                            "background-color : {}".format(color))
 
-"""玩家信息"""
-class player():
-    #储存累计回合数
+
+class Player():
+    """
+    玩家信息
+    """
+    # 储存累计回合数
     round = 0
     round_now = None
-    def __init__(self,player_number):
+
+    def __init__(self, player_number):
         # 储存身分组信息
         self.id = None
-        self.flag = ['red','blue','yellow','black']
+        self.flag = ['red', 'blue', 'yellow', 'black']
         self.chess_map = []
         self.player_chess(player_number)
         self.player_number = player_number
         self.player_round()
-    """玩家的棋子信息"""
-    def player_chess(self,player_number):
+
+    def player_chess(self, player_number):
+        """玩家的棋子信息"""
         id = []
         for i in range(player_number):
             id.append(i)
-            #创建储存玩家拥有棋子的位置
-            self.chess_map = [[] for x in range(player_number)]
+
+            self.chess_map = [[]
+                              for x in range(player_number)]  # 创建储存玩家拥有棋子的位置
         self.id = id
         # print(self.chess_map)
-    """判断当前回合"""
+
     def player_round(self):
-        # 当前回合记录
-        self.round_now = self.round % self.player_number
+        """判断当前回合"""
+        self.round_now = self.round % self.player_number  # 当前回合记录
         self.round += 1
 
-"""地图初始化"""
-class map_init(Set_map):
+
+class MapInit(SetMap):
     def __init__(self):
-        super(map_init, self).__init__()
-    """扩展地图"""
+        """地图初始化"""
+        super(MapInit, self).__init__()
+
     def extend_map(self):
+        """扩展地图"""
         pass
-    """随机位置生成一座高山"""
+
     def define_terrain(self):
-        self.chess_name = str((random.randint(0,self.size_x - 1),random.randint(0,self.size_y - 1)))
+        """随机位置生成一座高山"""
+        self.chess_name = str(
+            (random.randint(0, self.size_x - 1), random.randint(0, self.size_y - 1)))
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = game()
+    w = Game()
     w.land.show()
     app.exec()
-
--- map.py
+++ map.py
@@ -2,52 +2,63 @@
 import sys
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QApplication,QWidget,QGridLayout, QPushButton)
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QPushButton)
 
-class Set_map(QWidget):
-    """地图属性初始化"""
+
+class SetMap(QWidget):
+
     def __init__(self):
-        super(Set_map, self).__init__()
+        """地图属性初始化"""
+        super(SetMap, self).__init__()
         # 地图初始大小
         self.size_x = 5
         self.size_y = 5
         # 地图储存
         self.cache = []
-        #调用Ui
+        # 调用Ui
         self.init_ui()
-    """ui界面初始化"""
+
     def init_ui(self):
+        """ui界面初始化"""
         self.setWindowTitle('blue and red war')
         self.grid = QGridLayout()
         self.setLayout(self.grid)
-        #设置主页面大小
+        # 设置主页面大小
         self.setGeometry(QtCore.QRect(720, 150, 681, 680))
-        #设置网格大小
+        # 设置网格大小
         self.grid.width = 54
         self.grid.highth = 130
         self.setMinimumSize(QtCore.QSize(self.grid.width, self.grid.highth))
         self.init_set()
-    """创建初始地图"""
+
     def init_set(self):
-        positions = [(i, j) for i in range(self.size_x) for j in range(self.size_y)]
+        """创建初始地图"""
+        positions = [(i, j) for i in range(self.size_x)
+                     for j in range(self.size_y)]
         name = [(i, j) for i in range(self.size_x) for j in range(self.size_y)]
-        name = map(str,name)
-        self.set(positions,name)
-    """地图生成器"""
-    def set(self, positions,name):
+        name = map(str, name)
+        self.set(positions, name)
+
+    def set(self, positions, name):
+        """
+        地图生成器
+        :param positions:
+        :param name:
+        """
         for position, name in zip(positions, name):
             # button = Chess(note = name)
             button = QPushButton(name)
-            #设置按钮与网格大小相同
-            button.setMinimumSize(QtCore.QSize(self.grid.width,self.grid.highth))
-            #设置字体隐藏，把字体用于sender()确定来源
+            # 设置按钮与网格大小相同
+            button.setMinimumSize(QtCore.QSize(
+                self.grid.width, self.grid.highth))
+            # 设置字体隐藏，把字体用于sender()确定来源
             button.setStyleSheet("color : rgb(0,0,0,0);\n"
                                  "background-color : rgb(255,255,255,255)")
             # #设置按钮定位
             # button.note = name
-            #添加按钮到布局
+            # 添加按钮到布局
             self.grid.addWidget(button, *position)
-            #将地图储存，便于后续操作调整
+            # 将地图储存，便于后续操作调整
             self.cache.append(button)
 
 # class Chess(QPushButton):
@@ -57,9 +68,9 @@ class Set_map(QWidget):
 #         self.note = note
 
 
-#测试ui界面
+# 测试ui界面
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    w = Set_map()
+    w = SetMap()
     w.show()
     app.exec()
