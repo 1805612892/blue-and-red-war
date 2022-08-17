a24a7507effd61468f69931688c75cc3329b84c5
youyou_pm10 <1805612892@qq.com>
2022-08-17 17:13:33 +0800

    update ninth

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class game():
     def __init__(self):
         self.land = map_init()
         self.init_extend_map()
+        self.player = player(2)
         self.listen()
+
 
     """扩展地图初始化"""
     def init_extend_map(self):
@@ -37,8 +39,9 @@ class game():
     """检查移动条件"""
     def check(self,sign = 0):
         site = None
+        premit = None
         for i in range(len(self.land.cache)):
-            if self.land.cache[i].text() == self.chess:# and self.land.cache[i] != disable
+            if self.land.cache[i].text() == self.chess:
                 site = i
                 #遍历禁止地形
                 for disable in self.disable:
@@ -49,7 +52,7 @@ class game():
             #检查为人类棋子
             if sign == 0:
                 #回合判定
-                if True:
+                if premit != None:
                     self.march(site)
             #检查为高山地形
             if sign == 1:
@@ -58,10 +61,13 @@ class game():
 
     """棋子移动"""
     def march(self,site):
+        #保证交替下棋
+        color = self.player.flag[self.player.id[self.player.round_now]]
+        self.player.player_round()
         # 安全检查通过，允许进军
         if site != None:
         #持续隐藏字体
-            self.flag(self.land.cache[site],'red')
+            self.flag(self.land.cache[site],color)
 
     """棋子染色"""
     def flag(self,object,color):
@@ -70,18 +76,28 @@ class game():
 
 """玩家信息"""
 class player():
+    #储存累计回合数
+    round = 1
+    round_now = None
     def __init__(self,player_number):
         # 储存身分组信息
         self.id = None
         self.flag = ['red','blue','yellow']
         self.player_chess(player_number)
+        self.player_number = player_number
+        self.player_round()
     """玩家的棋子信息"""
     def player_chess(self,player_number):
         id = []
         for i in range(player_number):
             id.append(i)
-        self.id = zip(id,self.flag)
-
+        # self.id = zip(id,self.flag)
+        self.id = id
+    """判断当前回合"""
+    def player_round(self):
+        # 当前回合记录
+        self.round_now = self.round % self.player_number
+        self.round += 1
 
 """地图初始化"""
 class map_init(Set_map):
